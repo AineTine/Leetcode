55d5f040141127ff26cf2498a403bb62db350ad5
TineAine <Tsos201@outlook.com>
2020-01-13 09:24:26 +0800

    更新

---
+++ src/com/aine/day_04/Demo_01.java
@@ -0,0 +1,31 @@
+/*
+给你一个整数数组 arr，请你帮忙统计数组中每个数的出现次数。
+如果每个数的出现次数都是独一无二的，就返回 true；否则返回 false。
+*/
+package com.aine.day_04;
+
+public class Demo_01 {
+
+	public static void main(String[] args) {
+		int[] arr = {1,2,2,1,1,3};
+		System.out.println(uniqueOccurrences(arr));
+	}
+	
+    public static boolean uniqueOccurrences(int[] arr) {
+        
+    	for(int i:arr) {
+    		
+    		
+    		
+    	}
+    	
+    	return true;
+    }
+
+}
+/*
+
+
+
+
+*/
